60bad9359f65127492ca122751e7d9c280a14a6d
Negi <93192631+GiiBot@users.noreply.github.com>
2026-01-05 21:53:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+module.exports = {
+  GUILD_ID: "ID_SERVER",
+  TICKET_CATEGORY_ID: "ID_CATEGORY_TICKET",
+  LOG_CHANNEL_ID: "ID_KENH_LOG",
 
+  STAFF_ROLE_IDS: [
+    "ID_ROLE_ADMIN",
+    "ID_ROLE_MOD"
+  ],
+
+  EMBED: {
+    COLOR: "#ff5555",
+    FOOTER: "LORD OF CIARA • Ticket System"
+  }
+};
